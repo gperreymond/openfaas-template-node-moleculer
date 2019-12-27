fa93530dc02efb24a42ffe46a638c924e98aad95
Gilles Perreymond <gperreymond@gmail.com>
2019-12-27 15:19:26 +0100

    feat(): add transporter rabbitmq

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const { ServiceBroker } = require('moleculer')
 const ApiService = require('moleculer-web')
 
+const { APP_RABBITMQ_URI, APP_RABBITMQ_PREFETCH } = process.env
+
+const configuration = {
+  moleculer: {
+    metrics: false,
+    logger: true,
+    validation: true,
+    transporter: {
+      type: 'AMQP',
+      options: {
+        url: APP_RABBITMQ_URI || 'amqp://infra:infra@rabbitmq.docker.localhost:5672',
+        eventTimeToLive: 5000,
+        prefetch: parseInt(APP_RABBITMQ_PREFETCH) || 1,
+        autoDeleteQueues: true
+      }
+    }
+  }
+}
+
 const broker = new ServiceBroker({
-  metrics: false,
-  logger: true,
-  validation: true,
+  ...configuration.moleculer,
   async started () {
     broker.logger.warn('Broker started')
   },
